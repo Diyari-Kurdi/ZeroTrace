367961af7a478f8711e57f8433e2542478e4a634
Diyari <65598554+Diyari-Kurdi@users.noreply.github.com>
2025-07-15 04:43:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+using ZeroTrace.Views;
+
+namespace ZeroTrace;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        About.Show();
+
+        AnsiConsole.Markup("Press [green]Enter[/] to exit...");
+        Console.ReadLine();
+    }
+}
---
+++ Views/About.cs
@@ -0,0 +1,46 @@
+﻿using Spectre.Console;
+
+namespace ZeroTrace.Views;
+
+public static class About
+{
+    public static void Show()
+    {
+
+        AnsiConsole.Write(
+            new FigletText("ZeroTrace")
+                .Centered()
+                .Color(Color.Red));
+
+        var panel = new Panel(new Markup("[bold]Secure File Deletion Tool[/]\n\n" +
+                             "Safely overwrites and deletes files using custom patterns like zero fill, " +
+                             "random data, byte shaking, and more.").LeftJustified())
+        {
+            Border = BoxBorder.Square,
+            Padding = new Padding(2, 1),
+            Header = new PanelHeader("About", Justify.Center)
+        }.Expand();
+
+        var table = new Table { Border = TableBorder.HeavyEdge };
+        table.AddColumn(new TableColumn("Key").LeftAligned());
+        table.AddColumn(new TableColumn("Value").LeftAligned());
+        table.HideHeaders();
+        table.AddRow("[yellow]Author[/]", "Diyari Ismael");
+        table.AddRow("[yellow]GitHub[/]", "[link=https://github.com/Diyari-Kurdi/ZeroTrace]https://github.com/Diyari-Kurdi/ZeroTrace[/]");
+        table.AddRow("[yellow]Version[/]", "v1.0.0");
+        table.AddRow("[yellow]License[/]", "MIT");
+
+        var image = new CanvasImage("shred16.png");
+        image.NoMaxWidth();
+
+
+        var mainGrid = new Grid();
+        mainGrid.AddColumn();
+        mainGrid.AddColumn();
+        mainGrid.AddRow(image, new Rows(panel.Expand(), table.Expand()));
+
+        AnsiConsole.Write(mainGrid);
+
+        AnsiConsole.WriteLine();
+    }
+}
